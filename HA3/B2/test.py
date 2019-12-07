75a1db8d7b8d9818db09e1a344c69799fcc24034
filahf <filahf94@gmail.com>
2019-12-07 14:11:36 +0100

    b2 done

---
+++ HA3/B2/test.py
@@ -0,0 +1,29 @@
+import numpy as np
+from pypoly import Polynomial
+
+def calculate_master(poly):
+    equations = np.array([[index[0] ** i for i in range(len(poly))] for index in poly])
+    values = np.array([index[1] for index in poly])
+    coef = np.linalg.solve(equations, values)
+    print('Secret:', coef[0])
+
+
+
+def main():
+    #coeffs for p1
+    p1_poly = [9, 19, 5]
+    #given shares from the other participants’ polynomials
+    received_shares = [ 37, 18, 40, 44, 28]
+    #[index,value] for each of the collabs
+    collabs = [[4, 1385],[5,2028]]
+    
+    
+    combined_shares = sum(p1_poly) + sum(received_shares)
+    collabs.append([1,combined_shares])
+    calculate_master(collabs)
+
+
+
+
+if __name__ == "__main__":
+    main()
