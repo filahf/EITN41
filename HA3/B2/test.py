417b146a45f6b599b56644437466dc42692d84cb
filahf <filahf94@gmail.com>
2019-12-08 12:02:29 +0100

    quiz b2 done

--- HA3/B2/test.py
+++ HA3/B2/test.py
@@ -1,5 +1,19 @@
 import numpy as np
 from pypoly import Polynomial
+#pip3 install PyPolynomial
+
+def main():
+    #coeffs for p1
+    p1_poly = [4,4,7,9]
+    #given shares from the other participants’ polynomials
+    received_shares = [34,52,36,34,35,39]
+    #[index,value] for each of the collabs
+    collabs = [[3, 2080], [6, 12469], [7, 19052]]
+    
+    
+    combined_shares = sum(p1_poly) + sum(received_shares)
+    collabs.append([1,combined_shares])
+    calculate_secret(collabs)
 
 def calculate_secret(poly):
     #https://stackoverflow.com/questions/44471236/python-code-for-lagrange-interpolation-determining-the-equation-of-the-polynom
@@ -7,24 +21,6 @@ def calculate_secret(poly):
     values = np.array([index[1] for index in poly])
     coef = np.linalg.solve(equations, values)
     print('Secret:', coef[0])
-
-
-
-def main():
-    #coeffs for p1
-    p1_poly = [9, 19, 5]
-    #given shares from the other participants’ polynomials
-    received_shares = [ 37, 18, 40, 44, 28]
-    #[index,value] for each of the collabs
-    collabs = [[4, 1385],[5,2028]]
-    
-    
-    combined_shares = sum(p1_poly) + sum(received_shares)
-    collabs.append([1,combined_shares])
-    calculate_secret(collabs)
-
-
-
 
 if __name__ == "__main__":
     main()
