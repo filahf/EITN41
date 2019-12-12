85a8e3e70c1b34f09373cd3c0eac7c503e806b3a
filahf <filahf94@gmail.com>
2019-12-12 20:28:29 +0100

    B2

---
+++ HA4/B2/Grade.py
@@ -0,0 +1,28 @@
+import requests
+import numpy as np 
+
+def send_req(bundled_url):
+    resp = requests.get(bundled_url,verify=False)
+    time = resp.elapsed.total_seconds()
+    if(resp.text.strip() == "1"):
+
+        print("Correct", bundled_url[bundled_url.find("signature"):])
+    #base_url = "“https://eitn41.eit.lth.se:3119/ha4/addgrade.php?name={}&grade={}&signature=".format(name,grade)
+    return time
+
+
+def find_signature(name,grade, iterations):
+    base_url = "https://eitn41.eit.lth.se:3119/ha4/addgrade.php?name={}&grade={}&signature=".format(name,grade)
+    rec_time = [0] * 16
+    characters = "0123456789abcdef"
+    for i in range(iterations):
+        for char in characters:
+            print(base_url + char)
+            rec_time[characters.find(char)] = send_req(base_url + char)
+        base_url += characters[np.argmax(rec_time)]
+
+    return base_url
+
+
+signature = find_signature("Kalle",5,20)
+send_req(signature)
